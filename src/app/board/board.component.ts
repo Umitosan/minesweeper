8ceb34b408d5f3943e05c9820336f544d1c6c4e2
Ben Williams and Dominic Brown <ben@>
2017-06-15 11:03:00 -0700

    add bombScore

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -19,13 +19,23 @@ export class BoardComponent implements OnInit {
     for (let row = 0; row < 10; row++) {
       let rowArr: any[] = [];
       for (let col = 0; col < 10; col++) {
-        let newTile: Tile = new Tile(col,row,"unclicked",false);
+        let newTile: Tile = new Tile(col,row,"unclicked",0,false);
         rowArr.push(newTile);
       }
       tmpBoardArr.push(rowArr);
     }
     this.gameBoard = tmpBoardArr;
     this.setBombs();
+    // traverse board and set bomb score
+    for (let i=0; i<10; i++) {
+      for (let j=0; j<10; j++) {
+        this.setBombScore(i,j);
+      }
+    }
+    // document.addEventListener('contextmenu', function(e) {  // right-click listener
+    //         console.log("right-click happened");
+    //         e.preventDefault();
+    // });
   } // end INIT
 
   randomIntFromInterval(min,max) {
@@ -33,40 +43,87 @@ export class BoardComponent implements OnInit {
   }
 
   setBombs() {
-    let totalBombs: number = 20;
+    let totalBombs: number = 11;
     while (totalBombs > 0) {
       let myRow: number = this.randomIntFromInterval(0,9);
       let myCol: number = this.randomIntFromInterval(0,9);
-      this.gameBoard[myRow][myCol].bomb = true;
-      totalBombs -= 1;
-      console.log("c:",this.gameBoard[myRow][myCol].tCol,"r:",this.gameBoard[myRow][myCol].tRow);
+      if ( this.gameBoard[myRow][myCol].bomb === false ) {
+        this.gameBoard[myRow][myCol].bomb = true;
+        totalBombs -= 1;
+        console.log("c:",this.gameBoard[myRow][myCol].tCol,"r:",this.gameBoard[myRow][myCol].tRow);
+      }
     }
   }
 
   tileClicked(someTile) {
     if (someTile.bomb === true) {
-      this.gameBoard[someTile.tRow][someTile.tCol].status = "bomb";
-      this.bombTriggered();
+      this.bombTriggered(someTile);
     } else if (someTile.status === "unclicked") {
-      this.gameBoard[someTile.tRow][someTile.tCol].status = "clean";
+      if (someTile.bombScore === 0) {
+        someTile.status = "clean";
+      } else {
+        // reveal number
+        someTile.status = "number";
+      }
     }
   }
 
-  bombTriggered() {
-    console.log("BOOOM!");
+  setBombScore(row, col) {
+    let foundBombScore: number = 0;
+    if (this.gameBoard[row][col + 1]) {
+      if (this.gameBoard[row][col + 1].bomb === true)  { foundBombScore += 1; }
+    }
+    if ( this.gameBoard[row + 1] && this.gameBoard[row + 1][col + 1]) {
+      if (this.gameBoard[row + 1][col + 1].bomb === true)  { foundBombScore += 1; }
+    }
+    if ( this.gameBoard[row + 1] && this.gameBoard[row + 1][col]) {
+      if (this.gameBoard[row + 1][col].bomb === true)  { foundBombScore += 1; }
+    }
+    if ( this.gameBoard[row + 1] && this.gameBoard[row + 1][col - 1]) {
+      if (this.gameBoard[row + 1][col - 1].bomb === true)  { foundBombScore += 1; }
+    }
+    if ( this.gameBoard[row][col - 1 ]) {
+      if (this.gameBoard[row][col - 1 ].bomb === true)  { foundBombScore += 1; }
+    }
+    if ( this.gameBoard[row - 1] && this.gameBoard[row - 1][col - 1]) {
+      if (this.gameBoard[row - 1][col - 1].bomb === true)  { foundBombScore += 1; }
+    }
+    if ( this.gameBoard[row - 1] && this.gameBoard[row - 1][col]) {
+      if (this.gameBoard[row - 1][col].bomb === true)  { foundBombScore += 1; }
+    }
+    if ( this.gameBoard[row - 1] && this.gameBoard[row - 1][col + 1]) {
+      if (this.gameBoard[row - 1][col + 1].bomb === true)  { foundBombScore += 1; }
+    }
+    this.gameBoard[row][col].bombScore = foundBombScore;
   }
 
-  findTileByXY(someCol, someRow) {
-    let foundTile: Tile;
+  bombTriggered(thisTile) {
+    console.log("BOOOM!");
+    this.showAllBombs();
+    this.gameBoard[thisTile.tRow][thisTile.tCol].status = "bombRed";
+  }
+
+  showAllBombs() {
     for (let i=0; i<10; i++) {
       for (let j=0; j<10; j++) {
-        if ( ((this.gameBoard[i][j].tRow === someRow)&&(this.gameBoard[i][j].tCol === someCol)) ) {
-          foundTile = this.gameBoard[i][j];
+        if (this.gameBoard[i][j].bomb === true) {
+          this.gameBoard[i][j].status = "bomb";
         }
       }
     }
-    return foundTile
   }
+
+  // findTileByXY(someCol, someRow) {
+  //   let foundTile: Tile;
+  //   for (let i=0; i<10; i++) {
+  //     for (let j=0; j<10; j++) {
+  //       if ( ((this.gameBoard[i][j].tRow === someRow)&&(this.gameBoard[i][j].tCol === someCol)) ) {
+  //         foundTile = this.gameBoard[i][j];
+  //       }
+  //     }
+  //   }
+  //   return foundTile
+  // }
 
 }
 
--- src/app/tile.model.ts
+++ src/app/tile.model.ts
@@ -1,5 +1,5 @@
 export class Tile {
   constructor (
-    public tCol: number, public tRow: number, public status: string, public bomb: boolean
+    public tCol: number, public tRow: number, public status: string, public bombScore: number, public bomb: boolean
   ) {}
 }
